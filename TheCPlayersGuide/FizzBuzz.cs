4c42bc89f4b388af2229cfb82612bbd5ff6263b8
Temo <temo.kiknavelidze@gmail.com>
2016-12-11 18:20:12 +0400

    FizzBuzz

---
+++ TheCPlayersGuide/FizzBuzz.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TheCPlayersGuide
+{
+    public class FizzBuzz
+    {
+        public static void Main_(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 != 0)
+                {
+                    Console.WriteLine("Fizz");
+                } else if (i % 5 == 0 && i % 3 != 0)
+                {
+                    Console.WriteLine("Buzz");
+                }else if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        } 
+    }
+}
