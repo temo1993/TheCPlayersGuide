95247b061f9320723f2c7aed9e80c2415cc2806a
Temo <temo.kiknavelidze@gmail.com>
2016-12-15 15:57:13 +0400

    cb

--- TheCPlayersGuide/ColoredBalls.cs
+++ TheCPlayersGuide/ColoredBalls.cs
@@ -3,22 +3,22 @@
     internal class ColoredBalls
     {
         public static void Main_(string[] args) {
-            Ball _football = new Ball(1, 1, new Color(255, 0, 0));
-            Ball _basketBall = new Ball(2, 2, new Color(200, 10, 10));
-            Ball _rugbyball = new Ball(1, 2, new Color(255, 255, 255));
-            _football.Throw();
-            _football.Throw();
-            _football.Throw();
-            System.Console.WriteLine(_football.ThrowCount());
-            _football.Pop();
-            System.Console.WriteLine(_football.ThrowCount());
+            Ball football = new Ball(1, 1, new Color(255, 0, 0));
+            Ball basketBall = new Ball(2, 2, new Color(200, 10, 10));
+            Ball rugbyball = new Ball(1, 2, new Color(255, 255, 255));
+            football.Throw();
+            football.Throw();
+            football.Throw();
+            System.Console.WriteLine(football.ThrowCount());
+            football.Pop();
+            System.Console.WriteLine(football.ThrowCount());
 
-            _basketBall.Throw();
-            _basketBall.Throw();
-            System.Console.WriteLine(_basketBall.ThrowCount());
+            basketBall.Throw();
+            basketBall.Throw();
+            System.Console.WriteLine(basketBall.ThrowCount());
             
-            _rugbyball.Throw();
-            System.Console.WriteLine(_rugbyball.ThrowCount());
+            rugbyball.Throw();
+            System.Console.WriteLine(rugbyball.ThrowCount());
             
 
 
