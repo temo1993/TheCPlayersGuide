cc77e28eec3dc0481388f195537dc6a2d35743ca
Temo <temo.kiknavelidze@gmail.com>
2016-12-15 16:04:58 +0400

    cb

--- TheCPlayersGuide/ColoredBalls.cs
+++ TheCPlayersGuide/ColoredBalls.cs
@@ -3,9 +3,9 @@
     internal class ColoredBalls
     {
         public static void Main_(string[] args) {
-            Ball football = new Ball(1, 1, new Color(255, 0, 0));
-            Ball basketBall = new Ball(2, 2, new Color(200, 10, 10));
-            Ball rugbyball = new Ball(1, 2, new Color(255, 255, 255));
+            var football = new Ball(1, 1, new Color(255, 0, 0));
+            var basketBall = new Ball(2, 2, new Color(200, 10, 10));
+            var rugbyball = new Ball(1, 2, new Color(255, 255, 255));
             football.Throw();
             football.Throw();
             football.Throw();
