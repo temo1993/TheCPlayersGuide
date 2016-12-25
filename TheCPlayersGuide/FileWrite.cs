3a943b8dd0702ed691d1d4efe1a0fa48b9ac6e68
Temo <temo.kiknavelidze@gmail.com>
2016-12-25 15:28:07 +0400

    I/O

---
+++ TheCPlayersGuide/FileWrite.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace TheCPlayersGuide
+{
+    public class FileWrite
+    {
+        public static void Main_(string[] args)
+        {
+            FileStream fileStream = File.OpenWrite("../../files/some_data.txt");
+            StreamWriter writer = new StreamWriter(fileStream);
+            writer.Write("Hello ");
+            writer.Write("and Goodbye ");
+            writer.Write("at the same time");
+            writer.Close();
+
+
+            FileStream fileStream2 = File.OpenRead("../../files/some_data.txt");
+            StreamReader reader = new StreamReader(fileStream2);
+
+//            Console.WriteLine(reader.ReadLine());
+            
+            // Read a single character at a time.
+            char nextCharacter = (char)reader.Read();
+            // Read multiple characters at a time.
+            char[] bufferToPutStuffIn = new char[2];
+            reader.Read(bufferToPutStuffIn, 0, 2);
+            string whatWasReadIn = new string(bufferToPutStuffIn);
+            // Read a full line at a time.
+            string restOfLine = reader.ReadLine();
+            reader.Close();
+
+
+            Console.WriteLine(nextCharacter);
+            Console.WriteLine(whatWasReadIn);
+            Console.WriteLine(restOfLine);
+        } 
+    }
+}
