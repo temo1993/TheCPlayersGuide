105c39bf1f42892acb2d6e445e876f76ba3e832c
Home <Home@ACERW4TAB>
2016-12-29 12:44:55 +0400

    Delegate test

---
+++ TheCPlayersGuide/MathDelegate.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheCPlayersGuide
+{
+    public delegate int MathDelegate(int a, int b);
+}
---
+++ TheCPlayersGuide/MathDelegateMain.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheCPlayersGuide
+{
+    class MathDelegateMain
+    {
+        public static void Main(string[] args)
+        {
+            MathDelegate mathOperation = Add;
+        }
+
+
+
+
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Power(int baseNumber, int exponent)
+        {
+        return (int)Math.Pow(baseNumber, exponent);
+        }
+    }
+}
