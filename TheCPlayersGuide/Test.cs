0bd7b70bdd4630905e67cb83ea1e38a1820bfadc
Temo Kiknavelidze <temo.kiknavelidze@gmail.com>
2017-05-03 16:17:58 +0400

    ...

---
+++ TheCPlayersGuide/Test.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TheCPlayersGuide
+{
+    public class ListAddRemove
+    {   
+            static void Main_(string[] args)
+            {
+                List<string> simpleList = new List<string>();
+
+                while (true)
+                {
+                    Console.WriteLine($"List contains: {string.Join(",", simpleList)}");
+                    Console.WriteLine("Enter command (+ item, - item, or -- to clear, to Exit Press N)):");
+                    Console.WriteLine("To add/remove all/remove items in the List press: +/--/-.");
+                    var userInput = Console.ReadLine();
+                    if (userInput != null)
+                    {
+                        string[] userInputStrings = userInput.Split(' ');
+                        if (userInput.Contains("+"))
+                        {
+                            for (int i = 1; i < userInputStrings.Length; i++)
+                            {
+                                simpleList.Add(userInputStrings[i]);
+                            } 
+                        }
+                        else if (userInput.Contains("--"))
+                        {
+                            simpleList.Clear();  
+                        }
+                        else if (userInput.Contains("-"))
+                        {
+                            for (int i = 1; i < userInputStrings.Length; i++)
+                            {
+                                simpleList.Remove(userInputStrings[i]);
+                            }
+                        }
+                        else if (string.Equals("N", userInput, StringComparison.OrdinalIgnoreCase))
+                        {
+                            Console.WriteLine("Bye! :)");
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
