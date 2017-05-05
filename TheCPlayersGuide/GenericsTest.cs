0df5d21a00ed10efa4407356304756f925232c3c
Temo Kiknavelidze <temo.kiknavelidze@gmail.com>
2017-05-05 22:42:45 +0400

    ...

---
+++ TheCPlayersGuide/GenericsTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TheCPlayersGuide
+{
+    public class GenericsTest
+    {
+        private static readonly List<string> TestList = new List<string>();
+
+        public static void Main_(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                TestList.Add("Item" + i);
+            }
+
+            TestList.ForEach(Console.WriteLine);
+            TestList.Insert(10, "Item10");
+            var elementAt = TestList.ElementAt(10);
+            Console.WriteLine("__________");
+            Console.WriteLine(elementAt);
+            TestList.RemoveAt(0);
+            Console.WriteLine("__________");
+            TestList.ForEach(Console.WriteLine);
+            TestList.Clear();
+            Console.WriteLine("List after cleal method:");
+            TestList.ForEach(Console.WriteLine);
+            // Collection initializer syntax
+            List<int> someNumbersInAList = new List<int> { 14, 24, 37 };
+            Console.WriteLine("__________");
+            someNumbersInAList.ForEach(Console.WriteLine);
+
+
+            Dictionary<string, int> phoneBook = new Dictionary<string, int>
+            {
+                ["Gates, Bill"] = 5550100,
+                ["Zuckerberg, Mark"] = 5551438
+            };
+            var billsNumber = phoneBook["Gates, Bill"];
+            Console.WriteLine(billsNumber);
+        } 
+    }
+}
---
+++ TheCPlayersGuide/PracticeList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+
+namespace TheCPlayersGuide
+{
+    public class PracticeList<T> : IEnumerable where T : class, IComparable, new()
+        // Any Type that is Class(Reference Type), implements IComparable, and has
+        // parameterless constuctor. (use _where T:struct_ to specify that T has to be value type i.e struct)
+        // When we use two gener. type parameters we can specify that one must be derived from another _where T : U_.
+        // So T has be a child of U
+    {
+        private T[] items;
+
+        public PracticeList()
+        {
+            items = new T[0];
+        }
+
+        public T GetItem(int index)
+        {
+            return items[index];
+        }
+
+        public void Add(T newItem)
+        {
+            T[] newItems = new T[items.Length + 1];
+            for (int index = 0; index < items.Length; index++)
+            {
+                newItems[index] = items[index];
+            }
+            newItems[newItems.Length - 1] = newItem;
+            items = newItems;
+        }
+
+        public T LoadObject(string fileName)
+        {
+            throw new NotImplementedException("Method has to be implemented!!!");
+        }
+        public IEnumerator GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
