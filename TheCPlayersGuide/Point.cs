11528451675d918b3e5c8545785856b72481ba1e
Temo Kiknavelidze <temo.kiknavelidze@gmail.com>
2017-05-06 21:21:27 +0400

    ...Events Test...

---
+++ TheCPlayersGuide/Point.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace TheCPlayersGuide
+{
+    public class Point
+    {
+        private double _x;
+        private double _y;
+        public double X
+        {
+            get => _x;
+            set
+            {
+                _x = value;
+                OnPointChanged();
+            }
+        }
+        public double Y
+        {
+            get => _y;
+            set
+            {
+                _y = value;
+                OnPointChanged();
+            }
+        }
+
+        public event EventHandler PointChanged;
+
+        private void HandlePointChanged(Point pointThatChanged)
+        {
+            // Do something in response to the point changing.
+        }
+
+        public void OnPointChanged()
+        {
+            PointChanged?.Invoke(this, EventArgs.Empty);
+            // Above code is the same as bellow (Called null propagation)
+//            if (PointChanged != null)
+//            PointChanged(this, EventArgs.Empty);
+        }
+
+        public void HandlePointChanged(object sender, EventArgs eventArgs)
+        {
+            // Do something intelligent when the point changes. Perhaps redraw the GUI,
+            // or update another data structure, or anything else you can think of.
+        }
+    }
+}
---
+++ TheCPlayersGuide/PointMain.cs
@@ -0,0 +1,21 @@
+﻿namespace TheCPlayersGuide
+{
+    public class PointMain
+    {
+        public static void Main_(string[] args)
+        {
+            Point point = new Point();
+
+            // Attaching handler to the event
+            point.PointChanged += point.HandlePointChanged;
+
+            //            To detach an event, you simply use the -= operator in a manner similar to attaching events:
+            //            point.PointChanged -= HandlePointChanged;
+            //            After this executes, HandlePointChanged will not be called when the PointChanged event occurs.
+
+            // Now if we change the point, the PointChanged event will get raised,
+            // and HandlePointChanged will get called.
+            point.X = 3;
+        }
+    }
+}
