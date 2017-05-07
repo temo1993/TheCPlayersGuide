f740f1183957532db1c5de4a41be0203ca2ab8a3
Temo Kiknavelidze <temo.kiknavelidze@gmail.com>
2017-05-07 23:36:03 +0400

    .. Chapter 34 Extension Methods..

---
+++ TheCPlayersGuide/LineCounter.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace TheCPlayersGuide
+{
+    internal class LineCounter
+    {
+        static void Main_(string[] args)
+        {
+            string directory = @"C:\Users\Temo\Source\Repos\TheCPlayersGuide\TheCPlayersGuide";
+            Console.WriteLine(CountLinesOfCode(directory));
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Totals up all of the lines of code in all of the files in a
+        /// particular directory. This method is used recursively, so 
+        /// any directory contained within this directory will be counted
+        /// up by another call to this method.
+        /// </summary>
+        /// <param name="directory"></param>
+        /// <returns></returns>
+        public static int CountLinesOfCode(string directory)
+        {
+            // Start at 0.
+            int count = 0;
+
+            // Go through each child directory that is contained within
+            // this directory and add up their lines of code.
+            foreach (string child in Directory.GetDirectories(directory))
+            {
+                count += CountLinesOfCode(child);
+            }
+
+            // Go through each file in this directory...
+            foreach (string file in Directory.GetFiles(directory))
+            {
+                // ... and if they're a .cs file...
+                if (file.EndsWith(".cs"))
+                {
+                    // include the number of lines in that file.
+                    count += CountLinesOfCodeInFile(file);
+                }
+            }
+
+            return count;
+        }
+
+
+        /// <summary>
+        /// Counts the number of lines in a file. This does not account
+        /// for blank lines or lines that are just comments.
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
+        public static int CountLinesOfCodeInFile(string file)
+        {
+            return File.ReadAllText(file).ParagraphCount();
+
+            // or alternatively...
+            // return File.ReadAllLines(file).Length;
+        }
+    }
+
+
+
+    /*        internal int CountLinesInFile(string pathToFile)
+            {
+                FileStream fileStream = null;
+                StreamReader sr = null;
+                var stringList = new List<string>();
+                try
+                {
+                    fileStream = File.OpenRead(pathToFile);
+                    sr = new StreamReader(fileStream);
+                    while (!sr.EndOfStream)
+                    {
+                        if (!string.IsNullOrEmpty(sr.ReadLine()))
+                        {
+                            stringList.Add(sr.ReadLine());
+                        }
+                    }
+                }
+                catch (FileNotFoundException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                catch (IOException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                finally
+                {
+                    fileStream?.Close();
+                    sr?.Close();
+                }
+                stringList.ForEach(Console.WriteLine);
+                return stringList.Count;
+            }*/
+}
+
+
+internal static class StringExtension
+{
+    /// <summary>
+    /// Counts the number of paragraphs in a string. Paragraphs
+    /// are assumed to be delimited by the new line character
+    /// ('\n').
+    /// </summary>
+    public static int ParagraphCount(this string text)
+    {
+        return text.Split('\n').Length;
+    }
+}
---
+++ TheCPlayersGuide/StringExtensions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+
+namespace TheCPlayersGuide
+{
+    internal static class StringExtensions
+    {
+        private static readonly Random Random = new Random();
+
+        internal static string ToRandomCase(this string text)
+        {
+            string result = "";
+            for (int index = 0; index < text.Length; index++)
+            {
+                if (Random.Next(2) == 0) 
+                    result += text.Substring(index, 1).ToUpper();
+                else
+                    result += text.Substring(index, 1).ToLower();
+            }
+            return result;
+        }
+
+        internal static int WordCount(this string stringToCountWordsIn)
+        {
+            return stringToCountWordsIn.Split(new char[0], StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+
+        /// <summary>
+        /// Splits given text into sentence and paragraph count
+        /// </summary>
+        /// <param name="text">String to count sentences and paragraphs in</param>
+        /// <returns>Tuple containing setence and paragraph quantity in given string</returns>
+        internal static (int sentenceCount, int paragraphCount) SentenceAndParagraphCount(this string text)
+        {
+            text = text.Replace("...", "");
+            var sentenceCount = text.Split('.').Length;
+            var paragraphCount = text.Split('\n').Length;
+            return (sentenceCount, paragraphCount);
+        }
+
+        public static void Main_(string[] args)
+        {
+            string userName = "Steve Jobs";
+            // Using extension method syntax it looks like these methods are internals of string original class
+            // But they are not. first one is converted into StringExtensions.ToRandomCase(text);
+            Console.WriteLine(userName.ToRandomCase());
+            Console.WriteLine(userName.WordCount());
+            
+            // Reading file
+            var readFile = File.ReadAllText("../../files/SomeText.txt");
+            Console.WriteLine($"SomeText.txt file conains words: {readFile.WordCount()}, " +
+                              $"sentences: {readFile.SentenceAndParagraphCount().sentenceCount}," +
+                              $" paragraphs: {readFile.SentenceAndParagraphCount().paragraphCount}");
+        }
+    }
+}
